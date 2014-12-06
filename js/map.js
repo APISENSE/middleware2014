f3b9911c9f7247a8c70e921d51f90bf5306a1285
Julien Duribreux <julien.duribreux@inria.fr>
2014-12-06 21:53:50 +0100

    feature(map): Setting default location on Bordeaux

--- js/map.js
+++ js/map.js
@@ -7,7 +7,7 @@ var mapHeat;
 function initialize() {
 	var mapOptions = {
 		zoom: 12,
-		center: new google.maps.LatLng(50.650, 3.083) // Bordeaux : 44.8333, -0.5667
+		center: new google.maps.LatLng(44.8333, -0.5667) // Bordeaux : 44.8333, -0.5667
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'),
