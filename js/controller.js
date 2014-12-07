1a8f46bf6ca811c2ba9a0b46fd08cdb9f273d0eb
Christophe Ribeiro <christo.ribeiro@gmail.com>
2014-12-07 19:56:43 +0100

    feat(design): display alert message above the map

--- js/controller.js
+++ js/controller.js
@@ -4,13 +4,17 @@ $(document).ready(function() {
 	var browerHeight = $(window).height(),
 		navHeight = $('.navbar-aps').height(),
 		formHeight = $('#wrapper-form').height(),
-		footerHeight = $('.footer').height()+30;
+		footerHeight = $('.footer').height()+60;
 
 	// Set map height
 	if(browerHeight>640) {
 		$('#map-canvas').height(browerHeight - navHeight - formHeight - footerHeight);
 		$('.container-fluid').addClass('nomargin stickytonav');
 	}
+
+	$(".close-alert").click(function() {
+  		$(".alert").hide();
+	});
 });
 
 // Deciders
